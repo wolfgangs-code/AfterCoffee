9e2829100611c0fbcd5602291aca43a39d2ca9ec
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-05-25 11:16:09 -0500

    fix $date returning 1

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ if (file_exists($apath)) {
     }
     # Get the modified date directly from the markdown file,
     # Then get the title and description for the HTML from it as well.
-    $date = date(isset(USERSET["dateFormat"]) ?? "Y-m-d", filemtime($apath));
+    $date = date(USERSET["dateFormat"] ?? "Y-m-d", filemtime($apath));
     $title = insideTag($html, "h1");
     $description = insideTag($html, "h2");
 } else {
