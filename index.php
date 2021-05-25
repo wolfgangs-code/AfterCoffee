19ead924e3f03b59cb76b8e95702521d1448d302
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-05-25 11:11:46 -0500

    improve getURL()

--- index.php
+++ index.php
@@ -16,8 +16,7 @@ function getURL($page)
 {
 	$url = "http";
     $url .= isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "s" : "";
-    $url .= "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
-    $url .= "?page={$page}";
+    $url .= "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}?page={$page}";
     return $url;
 }
 
