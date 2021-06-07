07208fd71227c0c245df6db23dc70164dccd60cc
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-06-07 15:24:46 -0500

    Allow new defaults

--- config/userset.php
+++ config/userset.php
@@ -3,7 +3,9 @@
 if(!is_file(__DIR__."/../meta.json")) {
 	$userset = json_decode(file_get_contents(__DIR__."/../resource/default.json"), true);
 } else {
-	$userset = json_decode(file_get_contents(__DIR__."/../meta.json"), true);
+	$def = json_decode(file_get_contents(__DIR__."/../resource/default.json"), true);
+	$usr = json_decode(file_get_contents(__DIR__."/../meta.json"), true);
+	$userset = array_merge($def, $usr);
 }
 
 define("USERSET", $userset);
