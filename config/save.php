6a9d0b261fbbed76ddbb4a69fcf34167175b0fce
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-06-07 15:12:10 -0500

    Fix config saving

--- config/save.php
+++ config/save.php
@@ -21,13 +21,18 @@ function bob($arr)
     return $data;
 }
 
-$old = json_decode(file_get_contents("../meta.json"), true);
-$final = array_merge($old, bob($_POST));
+if(is_file(__DIR__."/../meta.json")) {
+	$old = json_decode(file_get_contents(__DIR__."/../meta.json"), true);
+	$final = array_merge($old, bob($_POST));
+} else {
+	$final = bob($_POST);
+}
+
 print("<pre>");
 print_r($final);
 print("</pre>");
 
-$file = fopen("../meta.json", "w");
+$file = fopen(__DIR__."/../meta.json", "w");
 fwrite($file, json_encode($final, JSON_PRETTY_PRINT));
 fclose($file);
 
