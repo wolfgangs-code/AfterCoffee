2f388af32820cc3033cb0f25b0b25c0215c86479
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-06-22 09:15:27 -0500

    renamed default config to avoid confusion
    or accidental changes

--- config/userset.php
+++ config/userset.php
@@ -3,7 +3,7 @@
 if(!is_file(__DIR__."/../meta.json")) {
 	$userset = json_decode(file_get_contents(__DIR__."/../resource/default.json"), true);
 } else {
-	$def = json_decode(file_get_contents(__DIR__."/../resource/default.json"), true);
+	$def = json_decode(file_get_contents(__DIR__."/../resource/default.config"), true);
 	$usr = json_decode(file_get_contents(__DIR__."/../meta.json"), true);
 	$userset = array_merge($def, $usr);
 }
