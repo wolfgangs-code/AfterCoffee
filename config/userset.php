88346a6f0ae571d81bd0f35d97bf5acf8e581439
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-06-22 09:49:50 -0500

    Correctly use default

--- config/userset.php
+++ config/userset.php
@@ -1,7 +1,7 @@
 <?php
 
 if(!is_file(__DIR__."/../meta.json")) {
-	$userset = json_decode(file_get_contents(__DIR__."/../resource/default.json"), true);
+	$userset = json_decode(file_get_contents(__DIR__."/../resource/default.config"), true);
 } else {
 	$def = json_decode(file_get_contents(__DIR__."/../resource/default.config"), true);
 	$usr = json_decode(file_get_contents(__DIR__."/../meta.json"), true);
