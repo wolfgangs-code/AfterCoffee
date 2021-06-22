821e7d39003da1d9904790815f61006cd5c35823
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-06-22 09:14:39 -0500

    moved en_us to en

--- config/lang.php
+++ config/lang.php
@@ -2,7 +2,7 @@
 
 	require_once 'userset.php';
 
-	$def = json_decode(file_get_contents(__DIR__."/../resource/lang/en_us.json"), true);
+	$def = json_decode(file_get_contents(__DIR__."/../resource/lang/en.json"), true);
 	$usr = json_decode(file_get_contents(__DIR__."/../resource/lang/".USERSET["lang"].".json"), true);
 	$userlang = array_merge($def, $usr);
 
