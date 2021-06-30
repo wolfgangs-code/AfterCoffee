3388fc386715b44b69c2e92d2f419c221b51a0ac
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-06-30 00:16:36 -0500

    Added new plugin hook: onSave

--- editor/publish.php
+++ editor/publish.php
@@ -1,31 +1,49 @@
 <?php
 require_once '../auth/auth_check.php';
 
-function publishPage($text, $title) {
-	$path = "../pages/".$title;
-	if (strpos($title, "/") && !is_dir($path)) mkdir($path, 0777, true);
-	$newPage = fopen($path.".md", "w");
-	fwrite($newPage, $text);
+foreach (glob("../plugins/*.php") as $plugin) {
+    include $plugin;
+    $pluginClasses[] = basename($plugin, ".php");
+}
+define("AC_PLUGINS", $pluginClasses);
+
+function publishPage($text, $title)
+{
+    $path = "../pages/" . $title;
+    if (strpos($title, "/") && !is_dir($path)) {
+        mkdir($path, 0777, true);
+    }
+
+    $newPage = fopen($path . ".md", "w");
+    fwrite($newPage, $text);
     fclose($newPage);
-	# If the page is empty, delete it.
-	if (empty($text)) {
-		$errstr = "<!-- NOINDEX ERROR -->\n# There was an error deleting '{$title}'.\n";
-		$errstr.= "## Please delete this file manually.";
-		# If a page cannot be deleted, hide it and explain why.
-		$t = 2;
-		unlink(realpath($path.".md")) ?: publishPage($errstr, $title);
-		print("Successfully deleted '{$title}'.\nReturning to index in {$t} seconds...");
-		header("refresh:{$t};url=../");
-	} else {header("Location: ../?page=".$title);}
+
+    # TODO: Make firing plugin functions cleaner
+    foreach (AC_PLUGINS as $class) {
+        $plugin = new $class;
+        if (method_exists($plugin, "onSave")) {
+			call_user_func($plugin->onSave());
+        }
+    }
+
+    # If the page is empty, delete it.
+    if (empty($text)) {
+        $errstr = "<!-- NOINDEX ERROR -->\n# There was an error deleting '{$title}'.\n";
+        $errstr .= "## Please delete this file manually.";
+        # If a page cannot be deleted, hide it and explain why.
+        $t = 2;
+        unlink(realpath($path . ".md")) ?: publishPage($errstr, $title);
+        print("Successfully deleted '{$title}'.\nReturning to index in {$t} seconds...");
+        header("refresh:{$t};url=../");
+    } else {header("Location: ../?page=" . $title);}
 }
 
 if (isset($_SESSION['authorized']) && $_SESSION['authorized'] == 1) {
     // Logged in, ready to go.
-	publishPage($_POST['textbox'], $_POST['pageName']);
+    publishPage($_POST['textbox'], $_POST['pageName']);
+
 } else {
-	// You aren't logged in, but are trying to publish...
-	header('Location: ../'); // Boot back to the pages
+    // You aren't logged in, but are trying to publish...
+    header('Location: ../'); // Boot back to the pages
     exit;
 }
-
-?>
