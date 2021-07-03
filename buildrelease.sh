1d32937e1e64dd48bc30341803fd45d03f8ddf5c
Wolfgang de Groot <52136571+wolfgang-degroot@users.noreply.github.com>
2021-07-02 23:58:42 -0500

    Release build script

---
+++ buildrelease.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "AfterCoffee Release Builder"
+read -p "=What is the release tag? >" tag
+mkdir buildtmp
+
+folders=("auth" "config" "editor" "lib" "pages" "plugins" "resource")
+folders+=("beans.php" "index.php" "LICENSE")
+
+for f in "${folders[@]}"
+do
+	cp -r $f buildtmp
+done
+
+# Clean directories
+find buildtmp/pages -mindepth 1 -not -name 'default-index.md' -delete
+find buildtmp/plugins -type f -not -name 'customMarkdown.php'  -not -name 'dateFormat.php' -not -name 'directoryList.php' -delete
+find buildtmp -type f -name '*.gitkeep' -delete
+rm buildtmp/auth/auth_code.php
+
+# Build ZIP
+cd buildtmp
+zip -r "../AfterCoffee-"$tag".zip" ./* -q
+cd ..
+rm -rf buildtmp
+echo "Release $tag built!"
