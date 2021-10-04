dc067683260b5e6a16250f3fc6370836d1c6391e
unknown <52136571+wolfgang-degroot@users.noreply.github.com>
2021-10-04 16:01:38 -0500

    Repair transition with semantic tagged article

--- plugins/directoryList.php
+++ plugins/directoryList.php
@@ -86,7 +86,7 @@ class directoryList
     public function addInfo()
     {
         $n = "\n\t\t";
-        $txt = "{$n}<select name=\"page\" onchange=\"document.getElementById('body').className = ' pageOut'; location = '?page=' + this.options[this.selectedIndex].value;\" form=\"directoryList\">{$n}";
+        $txt = "{$n}<select name=\"page\" onchange=\"document.getElementsByTagName('article')[0].className = ' pageOut'; location = '?page=' + this.options[this.selectedIndex].value;\" form=\"directoryList\">{$n}";
         $txt .= $this->getFiles();
         $txt .= "</select>\n";
 		$txt .= "<noscript><form id=\"directoryList\" action=\"\" method=\"get\"><input type=\"submit\" value=\"Go\"></form></noscript>";
