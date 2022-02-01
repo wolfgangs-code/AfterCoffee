f49c34461ae78352e0df36ca7e50dd7c83b14a3b
unknown <52136571+wolfgang-degroot@users.noreply.github.com>
2022-02-01 14:31:42 -0600

    One less variable

--- config/lang.php
+++ config/lang.php
@@ -4,7 +4,6 @@
 
 	$def = json_decode(file_get_contents(__DIR__."/../resource/lang/en.json"), true);
 	$usr = json_decode(file_get_contents(__DIR__."/../resource/lang/".USERSET["locale"]["lang"].".json"), true);
-	$userlang = array_merge($def, $usr);
 
-	define("USERLANG", $userlang);
+	define("USERLANG", array_merge($def, $usr));
 ?>
--- config/userset.php
+++ config/userset.php
@@ -5,8 +5,7 @@ if(!is_file(__DIR__."/../meta.json")) {
 } else {
 	$def = json_decode(file_get_contents(__DIR__."/../resource/default.config"), true);
 	$usr = json_decode(file_get_contents(__DIR__."/../meta.json"), true);
-	$userset = array_merge($def, $usr);
 }
 
-define("USERSET", $userset);
+define("USERSET", array_merge($def, $usr));
 ?>
