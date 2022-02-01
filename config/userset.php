f97a165925df2489c4dcd7d4894884a2220064ec
unknown <52136571+wolfgang-degroot@users.noreply.github.com>
2022-02-01 14:43:21 -0600

    Handle missing meta.json

--- config/userset.php
+++ config/userset.php
@@ -2,10 +2,10 @@
 
 if(!is_file(__DIR__."/../meta.json")) {
 	$userset = json_decode(file_get_contents(__DIR__."/../resource/default.config"), true);
+	define("USERSET", $userset);
 } else {
 	$def = json_decode(file_get_contents(__DIR__."/../resource/default.config"), true);
 	$usr = json_decode(file_get_contents(__DIR__."/../meta.json"), true);
+	define("USERSET", array_merge($def, $usr));
 }
-
-define("USERSET", array_merge($def, $usr));
 ?>
