530cfcc984c29b5bff11cc710201bb84573d8255
wolfgang-degroot <52136571+wolfgang-degroot@users.noreply.github.com>
2022-03-03 21:39:51 -0600

    Fix Content-Type distinguishment

--- api/index.php
+++ api/index.php
@@ -7,10 +7,17 @@ switch ($Router->base)
 {
 	case "page":
 		$Page = new Page($Router->path);
+
+		// Heads up: This is tricky to test on browsers.
+		// Be absolutely sure of your 'Accept' header.
 		if (in_array("text/html", $Router->accept)) {
-			$Page->getHTML();
+			// If the user is specifically requesting HTML
+			$API->body = $Page->getHTML();
+			$API->contentType = "text/html";
 		} else {
-			$Page->getMarkdown();
+			// If not, markdown.
+			$API->body = $Page->getMarkdown();
+			$API->contentType = "text/markdown";
 		}
 		break;
 }
