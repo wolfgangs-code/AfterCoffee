5db5fa89128a56abc831c42056816561e32752df
wolfgang-degroot <52136571+wolfgang-degroot@users.noreply.github.com>
2022-03-03 22:12:53 -0600

    Check if page exists at start

--- src/Page.php
+++ src/Page.php
@@ -5,10 +5,11 @@ require_once(__DIR__ . "/../lib/Parsedown.php");
 require_once(__DIR__ . "/../lib/ParsedownExtra.php");
 class Page
 {
+	public $exists; //	true or false.
 	public $name; //	"page" OR "folder/page"
 	public $path; //	"/var/www/AfterCoffee/pages/folder/page.md"
 	private $md; //		A full Markdown file string.
-	private $dm; //		DateTime object
+	private $dm; //		DateTime object.
 
 	public function __construct($path = ["default-index"])
     {
@@ -16,6 +17,7 @@ class Page
 		$this->name = rtrim(implode("/", $path), "/");
 		// Set the path (canonical)
 		$this->path = substr(__DIR__, 0, -3) . "pages/{$this->name}.md";
+		$this->exists = file_exists($this->path);
     }
 
 	// Returns the raw markdown of a page.
@@ -25,7 +27,7 @@ class Page
 			return $this->md;
 		} else {
 			$path = $this->path;
-			$this->md = file_exists($path)? file_get_contents($path): false;
+			$this->md = $this->exists? file_get_contents($path): false;
 			return $this->md;
 		}
 	}
@@ -36,12 +38,12 @@ class Page
 		$Parsedown = new ParsedownExtra();
 		//
 		$markdown = $this->getMarkdown();
-		// Return false if file does not exist
+		// Return false if file does not exist.
 		if ($markdown === false) {
 			return false;
 		} else {
 			$rawHTML = $Parsedown->setBreaksEnabled(true)->text($markdown);
-			// Return raw until PluginManager is rewritten
+			// Return raw until PluginManager is rewritten.
 			return $rawHTML;
 		}
 	}
@@ -50,9 +52,15 @@ class Page
 	public function getDateModified()
 	{
 		if (isset($this->dm)) {
+			// If we've already known the date modified.
+			return $this->dm;
+		} elseif ($this->exists) {
+			// If we need to find a date for an existing file.
+			$this->dm = new DateTime(filemtime($this->path));
 			return $this->dm;
 		} else {
-			$this->dm = new DateTime(filemtime($this->path));
+			// Default to now for non-existant files.
+			$this->dm = new DateTime("now");
 			return $this->dm;
 		}
 	}
