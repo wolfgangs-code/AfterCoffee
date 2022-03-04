78c97c0202be56c1f0cb3900c12422f1511797dd
wolfgang-degroot <52136571+wolfgang-degroot@users.noreply.github.com>
2022-03-03 20:51:03 -0600

    Begin API class

--- api/index.php
+++ api/index.php
@@ -1,3 +1,18 @@
 <?php
 require_once(__DIR__."/../src/API.php");
+$Router = new Router();
+$API = new API();
+
+switch ($Router->base)
+{
+	case "page":
+		$Page = new Page($Router->path);
+		if (in_array("text/html", $Router->accept)) {
+			$Page->getHTML();
+		} else {
+			$Page->getMarkdown();
+		}
+		break;
+}
+$API->respond();
 ?>
--- src/API.php
+++ src/API.php
@@ -1,12 +1,38 @@
 <?php
 require_once("Router.php");
 require_once("Page.php");
-$Router = new Router();
 
-switch ($Router->base)
+class API
 {
-	case "page":
-		$page = new Page($Router->path);
-		break;
+	// Universal Responses
+	public $statusCode; //	e.g. "200", "404", "500"
+	public $contentType; //	e.g. "text/html", "application/json"
+	// Page
+	public $lastModified; //e.g. (Unix Timestamp)
+
+	public function __construct()
+	{
+		$this->statusCode = 200;
+		$this->contentType = "application/json";
+	}
+
+	// Send out the final response.
+	public function respond()
+	{
+		// HTTP response code
+		http_response_code($this->statusCode);
+
+		// Use the 'Content-Type' header.
+		#header("Content-Type: {$this->contentType}; ; charset=utf-8");
+
+		// If set, use the 'Last-Modified' header.
+		if (isset($this->lastModified)) {
+			$Date = new DateTime($this->lastModified);
+			header("Last-Modified: {$Date->format(DATE_RFC7231)}");
+		}
+
+		// Finally, *wink*
+		header("X-Powered-By: AfterCoffee");
+	}
 }
 ?>
